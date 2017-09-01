db938195ecc1431050d3568d7b19e167ece2aef1
tcooper8 <coopetyl@live.com>
2017-09-01 16:00:24 -0600

    Updated logging on response for handler

--- handler/handlers.go
+++ handler/handlers.go
@@ -7,6 +7,7 @@ import (
   "github.com/tcooper8/thermostat-api/db"
   "github.com/tcooper8/thermostat-api/model"
   "strconv"
+  "log"
   "net/http"
 )
 
@@ -17,6 +18,11 @@ type Handler struct {
 func writeJsonResponse(resp http.ResponseWriter, bytes []byte) {
   resp.Header().Set("Content-Type", "application/json; charset=UTF-8")
   resp.Write(bytes)
+}
+
+func httpError(req *http.Request, resp http.ResponseWriter, err string, code int) {
+  log.Printf("Response %v with %v:%v", req.URL, err, code)
+  http.Error(resp, err, code)
 }
 
 func (handler *Handler) Init(db *db.Db) {
@@ -68,17 +74,17 @@ func getLimit(maxValue, defaultValue int, req *http.Request) (int, error) {
 func (handler *Handler) GetThermostats(resp http.ResponseWriter, req *http.Request) {
   limit, err := getLimit(100, 10, req)
   if err != nil {
-    http.Error(resp, err.Error(), http.StatusBadRequest)
+    httpError(req, resp, err.Error(), http.StatusBadRequest)
   }
   offset, err := getOffset(0, req)
   if err != nil {
-    http.Error(resp, err.Error(), http.StatusBadRequest)
+    httpError(req, resp, err.Error(), http.StatusBadRequest)
   }
 
   thermostats := handler.db.FindAll(limit, offset)
   bytes, err := json.Marshal(thermostats)
   if err != nil {
-    http.Error(resp, err.Error(), http.StatusInternalServerError)
+    httpError(req, resp, err.Error(), http.StatusInternalServerError)
   }
 
   writeJsonResponse(resp, bytes)
@@ -88,18 +94,18 @@ func (handler *Handler) GetThermostat(resp http.ResponseWriter, req *http.Reques
   vars := mux.Vars(req)
   id, err := uuid.FromString(vars["uuid"])
   if err != nil {
-    http.Error(resp, err.Error(), http.StatusBadRequest)
+    httpError(req, resp, err.Error(), http.StatusBadRequest)
     return
   }
   thermostat := handler.db.Find(id)
   if thermostat == nil {
-    http.Error(resp, "Thermostat not found.", http.StatusNotFound)
+    httpError(req, resp, "Thermostat not found.", http.StatusNotFound)
     return
   }
 
   bytes, err := json.Marshal(thermostat)
   if err != nil {
-    http.Error(resp, err.Error(), http.StatusInternalServerError)
+    httpError(req, resp, err.Error(), http.StatusInternalServerError)
   }
 
   writeJsonResponse(resp, bytes)
@@ -109,28 +115,28 @@ func (handler *Handler) PatchThermostat(resp http.ResponseWriter, req *http.Requ
   vars := mux.Vars(req)
   id, err := uuid.FromString(vars["uuid"])
   if err != nil {
-    http.Error(resp, err.Error(), http.StatusBadRequest)
+    httpError(req, resp, err.Error(), http.StatusBadRequest)
     return
   }
 
   decoder := json.NewDecoder(req.Body)
   defer req.Body.Close()
   if req.Body == nil {
-    http.Error(resp, "Expected body to be non-null.", http.StatusBadRequest)
+    httpError(req, resp, "Expected body to be non-null.", http.StatusBadRequest)
   }
 
   patch := model.ThermostatPatch{}
   err = decoder.Decode(&patch)
   if err != nil {
-    http.Error(resp, err.Error(), http.StatusBadRequest)
+    httpError(req, resp, err.Error(), http.StatusBadRequest)
   }
 
   updated, badRequest := handler.db.Patch(id, patch)
   if badRequest != nil {
-    http.Error(resp, badRequest.Error(), http.StatusBadRequest)
+    httpError(req, resp, badRequest.Error(), http.StatusBadRequest)
   } else if !updated {
-    http.Error(resp, "Thermostat not found.", http.StatusNotFound)
+    httpError(req, resp, "Thermostat not found.", http.StatusNotFound)
   } else {
-    http.Error(resp, "", http.StatusNoContent)
+    httpError(req, resp, "", http.StatusNoContent)
   }
 }
