23fba39e9c9f0a183a315e311ad87ec1158b22ba
tcooper8 <coopetyl@live.com>
2017-09-01 15:22:54 -0600

    Format fixes with go vet

--- main.go
+++ main.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-  "github.com/satori/go.uuid"
-  "math/rand"
-  "github.com/tcooper8/thermostat-api/model"
+	"github.com/satori/go.uuid"
 	"github.com/tcooper8/thermostat-api/db"
+	"github.com/tcooper8/thermostat-api/model"
 	"github.com/tcooper8/thermostat-api/server"
+	"math/rand"
 )
 
 func main() {
-  // This is all dummy data for the in-memory data store.
-  // This should be replaced with a real database implementation.
-  var thermostats = []model.Thermostat {
-    model.Thermostat {
-      uuid.NewV4(),
-      "bob1",
-      rand.Float64() * 20 + 70,
-      "off",
-      rand.Float64() * 20 + 70,
-      rand.Float64() * 20 + 70,
-      "auto",
-    },
-    model.Thermostat {
-      uuid.NewV4(),
-      "bob2",
-      rand.Float64() * 20 + 70,
-      "heat",
-      rand.Float64() * 20 + 70,
-      rand.Float64() * 20 + 70,
-      "auto",
-    },
-    model.Thermostat {
-      uuid.NewV4(),
-      "bob3",
-      rand.Float64() * 20 + 70,
-      "off",
-      rand.Float64() * 20 + 70,
-      rand.Float64() * 20 + 70,
-      "auto",
-    },
-    model.Thermostat {
-      uuid.NewV4(),
-      "bob4",
-      rand.Float64() * 20 + 70,
-      "cool",
-      rand.Float64() * 20 + 70,
-      rand.Float64() * 20 + 70,
-      "auto",
-    },
-    model.Thermostat {
-      uuid.NewV4(),
-      "bob5",
-      rand.Float64() * 20 + 70,
-      "off",
-      rand.Float64() * 20 + 70,
-      rand.Float64() * 20 + 70,
-      "auto",
-    },
-  }
+	// This is all dummy data for the in-memory data store.
+	// This should be replaced with a real database implementation.
+	var thermostats = []model.Thermostat{
+		model.Thermostat{
+			uuid.NewV4(),
+			"bob1",
+			rand.Float64()*20 + 70,
+			"off",
+			rand.Float64()*20 + 70,
+			rand.Float64()*20 + 70,
+			"auto",
+		},
+		model.Thermostat{
+			uuid.NewV4(),
+			"bob2",
+			rand.Float64()*20 + 70,
+			"heat",
+			rand.Float64()*20 + 70,
+			rand.Float64()*20 + 70,
+			"auto",
+		},
+		model.Thermostat{
+			uuid.NewV4(),
+			"bob3",
+			rand.Float64()*20 + 70,
+			"off",
+			rand.Float64()*20 + 70,
+			rand.Float64()*20 + 70,
+			"auto",
+		},
+		model.Thermostat{
+			uuid.NewV4(),
+			"bob4",
+			rand.Float64()*20 + 70,
+			"cool",
+			rand.Float64()*20 + 70,
+			rand.Float64()*20 + 70,
+			"auto",
+		},
+		model.Thermostat{
+			uuid.NewV4(),
+			"bob5",
+			rand.Float64()*20 + 70,
+			"off",
+			rand.Float64()*20 + 70,
+			rand.Float64()*20 + 70,
+			"auto",
+		},
+	}
 
-  app := server.App{}
-  db := &db.Db{}
+	app := server.App{}
+	db := &db.Db{}
 
-  db.Init()
-  db.Thermostats = thermostats
-  app.Init(db)
+	db.Init()
+	db.Thermostats = thermostats
+	app.Init(db)
 
-  app.Run(":8080")
+	app.Run(":8080")
 }
